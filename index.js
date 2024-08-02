ec2e4a99aeab58a0550d11fbf8ea8a5060ce94fa
Tundoor <thandwem045@gmail.com>
2024-08-02 09:09:37 +0200

    linked database

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 
-const inputFieldEl = document.getElementById("input-field")
+const appSettings = {
+    databaseUrl: "https://sdf10-9f177-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+
+const inputFieldEl = document.getElementById("input-field")   //Anything written in input field is console logged
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
+    push(shoppingListInDB, inputValue)
+
     console.log(inputValue)
 })
